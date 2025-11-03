d225f64bf124ce2dbf3130272249bc0e26f3f8e7
alexMartJu <alex.mart.juan@gmail.com>
2025-11-03 16:17:01 +0100

    Setup Swagger and global validation + add required dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Pipes de validación global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina campos no declarados en DTO
+      transform: true, // transforma tipos automáticamente
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const isDev = process.env.NODE_ENV !== 'production';
+
+  // Swagger solo en desarrollo
+  if (isDev) {
+    const config = new DocumentBuilder()
+      .setTitle('PoIQuest')
+      .setDescription('Documentación de NestJs de la aplicación PoIQuest')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
+    console.log('Swagger habilitado en /docs');
+  } else {
+    console.log('Swagger deshabilitado en producción');
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
